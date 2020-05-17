f818dd7e5611653ec68fee606333c08dbcfb0a18
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-17 23:49:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+dwarve_names = []
+long_calls = []
+
+
+def roll_call_dwarves(dwarves_names)
+    index = 0
+    dwarves_names.each_with_index {|name, index| print "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.capitalize
+    binding.pry
+  end
+  planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long_calls)
+  long_calls.any? {|word| word.length > 4}
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find {|type| cheese_types.include?(type)}
 end
